95fb714d40de2486b53af9fb7028e0dda0e1de9b
Ashan Perera <ashantiwankaperera@gmail.com>
2024-01-01 21:17:39 +0530

    svg  loader added

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import './styles.css'
 import Image from './react-image.png'
+import Logo from './react-logo.svg'
 
 export const App = () => {
     return <>
     <h1>React TypeScript Webpack</h1>
     <img src={Image} alt="React Image" width={300} height={"auto"} />
+    <br/>
+    <img src={Logo} alt="React Image" width={300} height={"auto"} />
     </>
 }
