c16495fc2022ee02d29fa2d860f51261bcb231cd
Ashan Perera <ashantiwankaperera@gmail.com>
2024-01-01 18:35:58 +0530

    image loader added

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 import './styles.css'
+import Image from './react-image.png'
 
 export const App = () => {
-    return <h1>React TypeScript Webpack</h1>
+    return <>
+    <h1>React TypeScript Webpack</h1>
+    <img src={Image} alt="React Image" width={300} height={"auto"} />
+    </>
 }
